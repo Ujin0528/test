734c80dbe500e3f6fb96037538edc050f6711311
Ujin0528 <128702337+Ujin0528@users.noreply.github.com>
2023-04-09 19:18:54 +0900

    C언어 2023.04.09

---
+++ C 반복문.c
@@ -0,0 +1,29 @@
+//반복문 for , while , do - while , 
+// do - while 은 do에 있는걸 실행하고 while로 검사후 do로 보내던 끝내던 선택
+//true일 경우 반복 false일 경우 반복문 종료
+#include<stdio.h>
+#pragma warning(disable:4996)
+int main()
+{
+	/*int num;
+	printf("붕어빵 개수를 입력하세요");
+	scanf("%d",&num);
+	
+	while(num>0){
+		printf("붕어빵하나굽기 성공\n");
+		num -= 1 ;  //== num=num-1
+	}*/
+//---------------------------------------------------------------------------
+	/*
+	double num, sum=0.0;                              //22번째줄 sum=sum+num이기 때문에 sum은 값이 없기에 실행이 안됨 그렇기에 sum=0.0이라는 값을 넣어줌
+	do {
+		printf("더할 숫자를 입력하세요 (그만두기=0) : ");
+		scanf("%lf", &num);
+		sum += num;
+	} while (num != 0.0);
+		printf("입력하셨던 숫자의 합은 %.2lf입니다",sum);
+		*/
+
+
+		return 0;
+}
