df37b1ca3286de1ad618e48fe36f90626d7abbc9
Ujin0528 <128702337+Ujin0528@users.noreply.github.com>
2023-05-02 23:45:51 +0900

    3항 연산자,if-else문을 이용한 짝수 홀수 판단 프로그램

--- C EP10.c
+++ C EP10.c
@@ -1,20 +1,29 @@
 #include<stdio.h>
 #pragma warning(disable:4996)
-int main()
-{
-	int num1;
-	printf("숫자를 입력하세요 : ");
-	scanf("%d", &num1);
-	if (num1 % 2 == 0) {
-		printf("짝수입니다.");
-	}
-	else
-		printf("홀수입니다.\n");
-		
-	int num2;
-	printf("숫자를 입력하세요 : ");
-	scanf("%d", &num2);
-	
-	(num2 % 2 == 0) ? printf("짝수입니다.") : printf("홀수입니다."); //a?b:c : a가 맞다면b, 아니면 c를 실행
+
+int main() {
+	//3항 연산자를 이용한 짝수 홀수 판단프로그램
+	/*int num;
+	do {
+		printf("짝수 홀수 판단 프로그램\n");
+		printf("숫자를 입력하세요('0'프로그램 종료) : ");
+		scanf("%d", &num);
+		if (num==0)
+			break;
+		(num % 2 == 0 ? printf("짝수입니다.\n") : printf("홀수입니다.\n"));
+
+	} while (1);
+	printf("프로그램을 종료합니다."); */
+	 
+	//if else를 사용한 짝수 홀수 판단프로그램
+	int num;
+		printf("숫자를 입력하세요 : ");
+		scanf("%d", &num);
+		if (num % 2 == 0) {
+			printf("짝수입니다.\n");
+		}
+		else {
+			printf("홀수입니다.\n");
+		}
 		return 0;
 }
